d86220a1260c72d7101a1bcad5e44363e39bbc49
user <user@DESKTOP-5L8E5H1>
2019-07-05 12:31:54 +0530

    code send

---
+++ src/main/java/com/app/SpringBootExternalDatabaseH2Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExternalDatabaseH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExternalDatabaseH2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/runner/ConsoleRunner.java
@@ -0,0 +1,27 @@
+package com.app.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.app.model.Product;
+import com.app.repo.ProductRepository;
+
+@Component
+public class ConsoleRunner implements CommandLineRunner {
+
+	@Autowired
+	private ProductRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println(repo.getClass().getName());
+		Product p=repo.save(new Product("A",2.3));
+		System.out.println(p.getProdId());
+
+		repo.save(new Product("B",3.3));
+		repo.save(new Product("C",4.4));
+	}
+
+}
